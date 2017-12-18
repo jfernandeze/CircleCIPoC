f166397143f30eeb687268fc17baccbca99a02ef
Luis de Santiago Buey <luis.santiago@arcmedia.ch>
2017-12-18 11:49:15 +0100

    ARCDIALOG-18: Add ValueController

---
+++ src/WebApi/Controllers/ValueController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DialogWeaver.WebApi.Controllers
+{
+    /// <summary>
+    /// Controller for Values
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+    [Route("api/value")]
+    public class ValueController : Controller
+    {
+        private static readonly IList<string> Values = new List<string>();
+
+        /// <summary>
+        /// Get all values
+        /// </summary>
+        /// <returns>All values</returns>
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Values);
+        }
+
+        /// <summary>
+        /// Adds the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        [HttpPost("{value}")]
+        public IActionResult Add([FromRoute]string value)
+        {
+            Values.Add(value);
+
+            return Accepted();
+        }
+    }
+}
