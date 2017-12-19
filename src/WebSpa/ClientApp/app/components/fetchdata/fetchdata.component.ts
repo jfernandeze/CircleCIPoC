a48c3a9b2874fc73c6cd684264dec5262f610ab1
Jose Fernandez Exposito <jose.fernandez@arcmedia.ch>
2017-12-19 11:31:29 +0100

    ARCDIALOG-19 component fetchdata call to WebApi

--- src/WebSpa/ClientApp/app/components/fetchdata/fetchdata.component.ts
+++ src/WebSpa/ClientApp/app/components/fetchdata/fetchdata.component.ts
@@ -1,20 +1,37 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'fetchdata',
     templateUrl: './fetchdata.component.html'
 })
-export class FetchDataComponent {
+export class FetchDataComponent implements OnInit {
 
     values: Array<string> = [];
+    newValue: string = "";
 
-    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-        
-        http.get<Array<string>>('http://localhost:5000/api/value').subscribe(result => {
+    constructor(private http: HttpClient) {
+                
+    }
+
+    ngOnInit(): void {
+        this.http.get<Array<string>>('http://localhost:5000/api/value').subscribe(result => {
             this.values = result;
         });
+    }
 
+
+    create() {
+
+        console.log("create");
+
+        this.http.post<void>('http://localhost:5000/api/value/' + this.newValue, null)
+            .subscribe(result => {
+                console.log(result);
+
+                this.ngOnInit();
+            }
+        );
     }
 }
 
