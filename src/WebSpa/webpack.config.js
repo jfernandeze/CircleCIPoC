5dba19252924b70359520df01c1ac8e909e5fcac
Jose Fernandez Exposito <jose.fernandez@arcmedia.ch>
2017-12-19 16:09:49 +0100

    ARCDIALOG-23: Included tslint into webpack

--- src/WebSpa/webpack.config.js
+++ src/WebSpa/webpack.config.js
@@ -17,10 +17,27 @@ module.exports = (env) => {
         },
         module: {
             rules: [
-                { test: /\.ts$/, use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader', 'angular2-router-loader'] : '@ngtools/webpack' },
-                { test: /\.html$/, use: 'html-loader?minimize=false' },
-                { test: /\.css$/, use: [ 'to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+                {
+                    test: /\.ts$/,
+                    enforce: 'pre',
+                    loader: 'tslint-loader'
+                },
+                {
+                    test: /\.ts$/,
+                    use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader', 'angular2-router-loader'] : '@ngtools/webpack'
+                },
+                {
+                    test: /\.html$/,
+                    use: 'html-loader?minimize=false'
+                },
+                {
+                    test: /\.css$/,
+                    use: ['to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize']
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg)$/,
+                    use: 'url-loader?limit=25000'
+                }
             ]
         },
         plugins: [new CheckerPlugin()]
